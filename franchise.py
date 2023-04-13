552e30ca023a1a2cd085654e3373d563f4db4b69
MPacris <mpacris@yahoo.com>
2023-04-13 16:57:40 -0500

    final version

--- franchise.py
+++ franchise.py
@@ -2,7 +2,10 @@
 from order_factory import OrderFactory
 from logger import logger
 
-
+#10 points): As a developer, I want to create a Franchise class with a place_order() method that will:
+# ask a user what food they would like to order
+#call the static OrderFactory.create_order() method to instantiate an order object.
+#all the logger.log_transaction() method to log the order to the log.txt file
 
 class Franchise:
     def __init__(self, location_number):
@@ -10,10 +13,8 @@ class Franchise:
 
 
     def place_order(self):
-        order = input(f"location- {self.location_number} choose from the following: \ntype 1 for Pizza, 2 for Pasta or 3 for Salad----   ")        
-      
-        orders = OrderFactory.create_order(order)
-            
+        order = input(f"location- {self.location_number} choose from the following: \ntype 1 for Pizza, 2 for Pasta or 3 for Salad----   ")
+        orders = OrderFactory.create_order(order)    
         logger.log_transaction(orders, self.location_number)
 
       
--- order_factory.py
+++ order_factory.py
@@ -3,13 +3,15 @@ from pizza import Pizza
 from salad import Salad
 
 
-# As a developer, I want to create an Order Factory class with a static create_order method.
+
 class OrderFactory:
 
 
 #As a developer, I want to utilize a Factory Pattern in the create_order() method to instantiate instances of the three different Order child classes.
 #This method should accept a string as a parameter (ex “Pizza”) and return the corresponding type of Order child class instantiation (ex Pizza() )
     @staticmethod
+# As a developer, I want to create an Order Factory class with a static create_order method.
+
     def create_order(order):
 
         if order == '1':
@@ -18,6 +20,7 @@ class OrderFactory:
             return Pasta()
         elif order == '3':
             return Salad()
+
        
 
 
